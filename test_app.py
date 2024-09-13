45941f089b0dea6fb6420a053f94e2849cb37b72
Ganesh-Evana <2000032069@kluniversity.in>
2024-09-13 11:23:09 +0530

    Initial commit

---
+++ test_app.py
@@ -0,0 +1,10 @@
+# test_app.py
+from app import add, subtract
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+
+def test_subtract():
+    assert subtract(2, 3) == -1
+    assert subtract(3, 1) == 2
